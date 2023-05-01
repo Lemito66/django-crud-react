aed9c4c7cf1606114aa3d08eb4cf035edd611103
Emill <lemito66@gmail.com>
2023-05-01 18:32:47 -0500

    Nuevo formato de app.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,21 @@
-
+import {BrowserRouter, Routes, Route, Navigate} from 'react-router-dom'
+import {TaskPage} from './pages/TaskPage'
+import {TaskFormPage} from './pages/TaskFormPage'
+import {Navigation} from './components/Navigation'
 function App() {
   
 
   return (
     <>
-      <h1>Hola mis brokis</h1>
+      <BrowserRouter>
+        <Navigation />
+        <Routes>
+          <Route path="/" element={<Navigate to="/tasks" />} />
+          <Route path="/" element={<TaskPage />} />
+          <Route path="/tasks" element={<TaskPage />} />
+          <Route path="/task-create" element={<TaskFormPage />} />
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }
