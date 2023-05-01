ab95494ca2171687b95cf9db584e4170cc1ebbd1
Emill <lemito66@gmail.com>
2023-05-01 17:10:22 -0500

    git ignore y quien puede acceder al api

--- django_crud_api/settings.py
+++ django_crud_api/settings.py
@@ -125,3 +125,9 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+# Quien puede hacer peticiones a la API
+# Cors autherization
+CORS_ALLOWED_ORIGINS = [
+    
+]
