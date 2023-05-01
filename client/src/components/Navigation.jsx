fb942a786f4b5626db57f9b2bd3664db72c8d778
Emill <lemito66@gmail.com>
2023-05-01 18:33:15 -0500

    componente de navegación

---
+++ client/src/components/Navigation.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+export function Navigation() {
+  return (
+    <div>
+        
+        <Link to="/"><h1>Task App</h1></Link>
+        <Link to="/tasks">Tasks</Link>
+        <Link to="/task-create">Create Task</Link>
+    </div>
+  )
+}
