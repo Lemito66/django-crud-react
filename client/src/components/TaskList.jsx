b3734dac5a3522556c68554d15e6268765262591
Emill <lemito66@gmail.com>
2023-05-15 15:44:33 -0500

    lista de tareas

---
+++ client/src/components/TaskList.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import { useEffect, useState } from "react";
+import { getAllTasks } from "../../api/tasks.api";
+import { TaskCard } from "./TaskCard";
+
+export function TaskList() {
+  const [tasks, setTasks] = useState([]);
+
+  useEffect(() => {
+    console.log("Se ha cargado la página");
+    async function loadTasks() {
+      const response = await getAllTasks();
+      setTasks(response.data);
+    }
+    loadTasks();
+  }, []);
+
+  return (
+    <div>
+      {tasks.map(({id, title, description, done}) => {
+        return (
+          <TaskCard
+            key={id}
+            id={id}
+            title={title}
+            description={description}
+            done={done}
+          />
+        );
+      })}
+    </div>
+  );
+}
