63d6e9c41a9fd3af6099406ab37a5836e1aac831
Emill <lemito66@gmail.com>
2023-05-16 18:14:55 -0500

    nuevo componente

--- client/src/components/TaskCard.jsx
+++ client/src/components/TaskCard.jsx
@@ -1,6 +1,11 @@
+import {useNavigate} from 'react-router-dom';
+
 export function TaskCard({id, title, description, done}){
+    const navigate = useNavigate();
     return(
-        <div key={id}>
+        <div style={{background: "#black"}} onClick={()=>{
+            navigate(`/tasks/${id}`);
+        }}>
             <h1>{title}</h1>
             <p>{description}</p>
             <p>{done}</p>
