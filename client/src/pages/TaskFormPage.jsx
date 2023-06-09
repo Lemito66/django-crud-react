df0755fd11b62c60d65c932328dcd53fb0bf807d
Emill <lemito66@gmail.com>
2023-06-09 12:10:38 -0500

    estilos y notificaciones

--- client/src/pages/TaskFormPage.jsx
+++ client/src/pages/TaskFormPage.jsx
@@ -8,6 +8,7 @@ import {
 } from "../../api/tasks.api";
 import { useNavigate, useParams } from "react-router-dom";
 import { useEffect, useState } from "react";
+import { toast } from "react-hot-toast";
 
 export function TaskFormPage() {
   const [task, setTask] = useState([]);
@@ -31,8 +32,16 @@ export function TaskFormPage() {
     if (params.id) {
       //console.log("actualizando")
       await updateTask(params.id, data);
+      toast.success("Tarea actualizada exitosamente");
     } else {
       await createTask(data);
+      toast.success("Tarea creada exitosamente", {
+        position: "top-center",
+        style: {
+          background: "#101010",
+          color: "#fff",
+        },
+      });
     }
     navigate("/tasks");
   });
@@ -41,6 +50,7 @@ export function TaskFormPage() {
     const accepted = window.confirm("¿Está seguro de eliminar esta tarea?");
     if (accepted) {
       await deleteTask(params.id);
+      toast.success("Tarea eliminada exitosamente");
       navigate("/tasks");
     }
   };
@@ -58,25 +68,38 @@ export function TaskFormPage() {
     loadTask();
   }, []);
   return (
-    <div>
+    <div className="max-w-xl mx-auto">
       <form onSubmit={onSubmit}>
         <input
           type="text"
           placeholder="Titulo"
           {...register("title", { required: true })}
+          className="bg-zinc-700 p-3 rounded-lg block w-full mb-3"
         />
         {errors.title && <span>El título es requerido</span>}
         <textarea
           rows="3"
           placeholder="Descripción"
           {...register("description", { required: true })}
+          className="bg-zinc-700 p-3 rounded-lg block w-full mb-3"
         ></textarea>
         {errors.description && <span>La descripción es requerida</span>}
         {/* <input type="checkbox" {...register("done", { required: true })}/>
         {errors.done && <span>El estado es requerido</span>} */}
-        <button>Guardar</button>
+        <button className="bg-indigo-500 p-3 rounded-lg block w-full mt-3">
+          Guardar
+        </button>
       </form>
-      {params.id && <button onClick={deleteTaskById}>Eliminar</button>}
+      {params.id && (
+        <div className="flex">
+          <button
+            className="bg-red-500 p-3 rounded-lg w-48 mt-3"
+            onClick={deleteTaskById}
+          >
+            Eliminar
+          </button>
+        </div>
+      )}
     </div>
   );
 }
