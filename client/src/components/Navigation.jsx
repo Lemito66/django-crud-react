1ae6e66aba41026bc76a687d12f00255879b4ff8
Emill <lemito66@gmail.com>
2023-06-09 12:17:21 -0500

    nombres en español

--- client/src/components/Navigation.jsx
+++ client/src/components/Navigation.jsx
@@ -5,11 +5,11 @@ export function Navigation() {
   return (
     <div className="flex justify-between py-3">
       <Link to="/">
-        <h1 className="font-bold text-3xl mb-4">Task App</h1>
+        <h1 className="font-bold text-3xl mb-4">Aplicación de Tareas</h1>
       </Link>
       {/* <Link to="/tasks">Tasks</Link> */}
       <button className="bg-indigo-500 px-3 py-2 rounded-lg">
-        <Link to="/task-create">Create Task</Link>
+        <Link to="/task-create">Crear Tarea</Link>
       </button>
     </div>
   );
