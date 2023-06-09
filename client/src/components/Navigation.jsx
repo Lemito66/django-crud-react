6ee84535c9785be16a15aa33f8bcc4708ccda6eb
Emill <lemito66@gmail.com>
2023-06-09 12:13:10 -0500

    prettier y estilos

--- client/src/components/Navigation.jsx
+++ client/src/components/Navigation.jsx
@@ -1,13 +1,16 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React from "react";
+import { Link } from "react-router-dom";
 
 export function Navigation() {
   return (
-    <div>
-        
-        <Link to="/"><h1>Task App</h1></Link>
-        <Link to="/tasks">Tasks</Link>
+    <div className="flex justify-between py-3">
+      <Link to="/">
+        <h1 className="font-bold text-3xl mb-4">Task App</h1>
+      </Link>
+      {/* <Link to="/tasks">Tasks</Link> */}
+      <button className="bg-indigo-500 px-3 py-2 rounded-lg">
         <Link to="/task-create">Create Task</Link>
+      </button>
     </div>
-  )
+  );
 }
